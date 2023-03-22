a69bd69225d5cfaf59f501a28de814191c8f773a
alrizkipascar <87491167+alrizkipascar@users.noreply.github.com>
2023-03-22 23:40:53 +0700

    tf serving

---
+++ api/main-tf-serving.py
@@ -0,0 +1,60 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, File, UploadFile
+import uvicorn
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+import requests
+
+app = FastAPI()
+endpoint = "http://localhost:8501/v1/models/potatoes_model:predict"
+MODEL = tf.keras.models.load_model("../Potatoe-Disease-Classification/models/1")
+CLASS_NAMES = ["Early Blight", "Late Blight","Healthy"]
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/ping")
+
+async def ping():
+    return "Hello, I am alive"
+
+def read_file_as_image(data) -> np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    return image
+
+@app.post("/predict")
+
+async def predict(
+    file: UploadFile = File(...)
+):
+    image = read_file_as_image(await file.read())
+    img_batch = np.expand_dims(image, 0)
+    json_data = {
+        "instances": img_batch.tolist()
+    }
+
+    response = requests.post(endpoint, json=json_data)
+    predictions = np.array(response.json()["predictions"][0])
+    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+    confidence = np.max(predictions[0])
+    return {
+        'class': predicted_class,
+        'confidence': float(confidence)
+    }
+    
+    
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='localhost', port=8000)
